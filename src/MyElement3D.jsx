193672d8f26615eddee7e00c11d101a549fa996c
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2024-03-18 20:31:58 +0900

    Geometry in Geometry

--- src/MyElement3D.jsx
+++ src/MyElement3D.jsx
@@ -1,18 +1,62 @@
+import { OrbitControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import * as THREE from "three"; // (N * Math.PI) / 180 => THREE.MathUtils.degToRad(N)
 
 function MyElement3D() {
-  const refMesh = useRef();
+  const refMesh1 = useRef();
+  const refMesh2 = useRef();
+  const refMesh3 = useRef();
   useFrame((state, delta) => {
-    refMesh.current.rotation.y += delta;
+    refMesh1.current.rotation.x -= delta;
+    refMesh2.current.rotation.y += delta;
+    refMesh3.current.rotation.y += delta;
   });
 
   return (
     <>
       <directionalLight position={[1, 1, 1]} />
-      <mesh ref={refMesh} rotation-y={(45 * Math.PI) / 180}>
+
+      <axesHelper scale={10} />
+      <OrbitControls />
+
+      <mesh
+        position-x={2}
+        rotation={[
+          THREE.MathUtils.degToRad(45),
+          THREE.MathUtils.degToRad(45),
+          0,
+        ]}
+        scale={[1, 1, 1]}
+      >
         <boxGeometry />
-        <meshStandardMaterial color="#e67e22" />
+        <meshStandardMaterial
+          color="#e67e22"
+          opacity={0.3}
+          transparent={true}
+        />
+        <axesHelper />
+
+        <mesh ref={refMesh1} scale={[0.5, 0.5, 0.5]}>
+          <torusGeometry />
+          <meshStandardMaterial color="pink" />
+        </mesh>
+        <mesh
+          ref={refMesh2}
+          scale={[0.5, 0.5, 0.5]}
+          rotation-z={THREE.MathUtils.degToRad(90)}
+        >
+          <torusGeometry />
+          <meshStandardMaterial color="skyblue" />
+        </mesh>
+        <mesh
+          ref={refMesh3}
+          scale={[0.5, 0.5, 0.5]}
+          rotation-x={THREE.MathUtils.degToRad(270)}
+        >
+          <torusGeometry />
+          <meshStandardMaterial color="lightgreen" />
+        </mesh>
       </mesh>
     </>
   );
